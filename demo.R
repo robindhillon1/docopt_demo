0edffde89676a023dad2b5f77eddf9c89d97ec8a
Robin Dhillon <robindhillon1@hotmail.com>
2022-11-18 16:03:51 -0800

    updated q5

--- demo.R
+++ demo.R
@@ -2,13 +2,13 @@
 # date: 2020-01-15
 
 "This script prints out docopt args.
-Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>] [<arg4>]
+Usage: demo.R <arg1> [<arg4>] --arg2=<arg2> [--arg3=<arg3>]
 
 Options:
 <arg>             Takes any value (this is a required positional argument)
+[<arg4>]	  Takes any value (this is an optional option)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
-<arg4>		  Takes any value (this is an optional option)
 " -> doc
 
 library(docopt)
